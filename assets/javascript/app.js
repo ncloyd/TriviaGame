2395caa390c64c7513deaca724431320c55c7b31
Nicole Cloyd <ncloyd@fm.northwestern.edu>
2017-10-12 18:02:33 -0500

    terrible

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,79 @@
 $(document).ready(function() {
+    // Entry page displaying start button
+    //upon clicking start trivia page is displayed
+    // Timer starts counting down fom 60 seconds
+
+    var timeleft = 30;
+    var downloadTimer = setInterval(function(){
+    timeleft--;
+    document.getElementById("countdown").textContent = "Time Remaining: " + timeleft;
+    if(timeleft <= 0)
+        clearInterval(downloadTimer);
+    },1000);
 
 
-// Entry page displaying start button
-	//upon clicking start trivia page is displayed
-	// Timer starts counting down fom 60 seconds
 
 
-$("#submit").click(function() {
-  var val = $('input[name=q12_3]:checked').val();
-  alert(val);
+
+    var correct = 0;
+    var wrong = 0;
+ 
+
+
+
+
+    function submit() {
+        var total = 5;
+        var score = 0;
+
+        var q1 = document.forms["quiz"]["q1"].value;
+        alert(q1);
+    };
+
+
+
+
+    // function checkAns() {
+    //     if ($("input[name=option]:checked").val() == allQuestions[currentquestion].correct) {
+    //         correct++;
+    //     };
+    // };
+
+    // setupOptions();
+
+    // $("#next").click(function() {
+    //     event.preventDefault();
+    //     checkAns();
+    //     currentquestion++;
+    // });
+
+    // if (currentquestion < allQuestions.length) {
+    //     setupOptions();
+    //     if (currentquestion == allQuestions.length - 1) {
+    //         $('#next').html("Submit");
+    //         $('#next').click(function() {
+    //             $("#result").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").hide();
+    //             $("#result").fadeIn(1500);
+    //         });
+
+    //     };
+
+    // };
 });
+
+
+
+
 
 
 // 8-10 questions displayed
-	// each question has 4 possible answers
-		// only 1 answer can be chosen, i.e. only one box checked at a time
+// each question has 4 possible answers
+// only 1 answer can be chosen, i.e. only one box checked at a time
+// all correct answers need to be counted
+// all incorrect answers need to be counted
+// all unanswered questions need to be counted
 
 
-// $( "input:checkbox" )
-
-
-
-// var answers1 = ["Ron", "Hermione", "Hedwig", "Dobey"];
-
-
-
-
-
-
-//When the timer hits zero
-	//Display Times Out!
-	//Display Correct Answer count
-	//Display Incorrect Answer count
-	//Display Unanswered count
-});
+//Display Correct Answer count
+//     //Display Incorrect Answer count
+//     //Display Unanswered count
